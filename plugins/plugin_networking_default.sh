b7b372867219ae3aabe065f461e94edd4049a016
os <ukrainger@gmail.com>
2022-07-02 10:15:25 -0400

    Network management. Make sure there is no active network management enabled by default.

--- plugins/plugin_networking_default.sh
+++ plugins/plugin_networking_default.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-source "${pluginDir}/plugin_networking_nm.sh"
+#source "${pluginDir}/plugin_networking_nm.sh"
+
+echo "#################################################################################################"
+echo "Note, there is no active network management plugin selected -> network state will not be impacted";
+echo "#################################################################################################"
+sleep 5s
+
+############### networking commands ########################
+
+#check whether networking is enabled at all. It returns nothing if not matched.
+function statusNetworkingEnabledCheckCommand { echo 1; }
+
+#check whether networking connectivity is available
+function statusConnectivityEnabledCheckCommand { echo 1; }
+
+#check whether network connection is established (not the same as networking check)
+function statusNetworkConnectedCheckCommand { echo 1; }
+
+#turn on network
+function connectNetworkCommand { sleep 1s; }
+
+#turn off network
+function disconnectNetworkCommand { sleep 1s; }
+
+############################################################
